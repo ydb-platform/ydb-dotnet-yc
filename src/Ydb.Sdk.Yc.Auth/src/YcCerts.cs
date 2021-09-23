220fd3d9d630a6b615fe0a6f23f9995c7a295e5a
Sergey Puchin <s.puchin@gmail.com>
2021-09-23 18:41:32 +0300

    Add default YC server certificate.

---
+++ src/Ydb.Sdk.Yc.Auth/src/YcCerts.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.FileProviders;
+
+namespace Ydb.Sdk.Yc
+{
+    public static class YcCerts {
+        public static X509Certificate2 GetDefaultServerCertificate() {
+            var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), "");
+
+            using (var stream = embeddedProvider.GetFileInfo("yc_default.pem").CreateReadStream())
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    return new X509Certificate2(memoryStream.ToArray());
+                }
+            }
+        }
+    };
+}
