73bba0a5503a96ed6494361b1156baf1a35be39c
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-02-27 18:15:07 +0300

    fix: invalid certificate

--- src/Ydb.Sdk.Yc.Auth/src/YcAuth.cs
+++ src/Ydb.Sdk.Yc.Auth/src/YcAuth.cs
@@ -1,17 +1,14 @@
-using System;
+namespace Ydb.Sdk.Yc;
 
-namespace Ydb.Sdk.Yc
+internal static class YcAuth {
+    public const string DefaultAudience = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
+    public const string MetadataUrl = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token";
+};
+
+internal class EmptyYcCredentialsProvider : Yandex.Cloud.Credentials.ICredentialsProvider
 {
-    internal static class YcAuth {
-        public const string DefaultAudience = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
-        public const string MetadataUrl = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token";
-    };
-
-    internal class EmptyYcCredentialsProvider : Yandex.Cloud.Credentials.ICredentialsProvider
+    public string GetToken()
     {
-        public string GetToken()
-        {
-            return "";
-        }
+        return "";
     }
 }
--- src/Ydb.Sdk.Yc.Auth/src/YcCerts.cs
+++ src/Ydb.Sdk.Yc.Auth/src/YcCerts.cs
@@ -3,16 +3,21 @@ using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.FileProviders;
 
-namespace Ydb.Sdk.Yc
-{
-    public static class YcCerts {
-        public static X509Certificate2 GetDefaultServerCertificate() {
-            var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), "");
+namespace Ydb.Sdk.Yc;
 
-            using var stream = embeddedProvider.GetFileInfo("yc_default.pem").CreateReadStream();
-            using var memoryStream = new MemoryStream();
-            stream.CopyTo(memoryStream);
-            return new X509Certificate2(memoryStream.ToArray());
-        }
-    };
+public static class YcCerts
+{
+    public static X509Certificate2Collection GetYcServerCertificates()
+    {
+        var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), "");
+
+        using var stream = embeddedProvider.GetFileInfo("Ydb.Sdk.Yc.YandexAllCAs.pkcs").CreateReadStream();
+        using var memoryStream = new MemoryStream();
+        stream.CopyTo(memoryStream);
+
+        var collection = new X509Certificate2Collection();
+        collection.Import(memoryStream.ToArray(), "yandex", X509KeyStorageFlags.Exportable);
+        
+        return collection;
+    }
 }
